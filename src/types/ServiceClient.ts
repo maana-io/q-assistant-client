65fe1208f9ff71247a555385fcd0f8ff198835a8
Darrell Lewis-Sandy <dlewissandy@gmail.com>
2022-07-08 16:03:46 -0700

    Added types to serviceClient

--- src/types/ServiceClient.ts
+++ src/types/ServiceClient.ts
@@ -1,1 +1,18 @@
-export type ServiceClient = {};
+import { Maybe } from '../schema';
+import { ID } from '../schema/scalars';
+import {
+  AssistantFunctionsFragment,
+  AssistantKindsFragment,
+} from './Fragments';
+import { WorkspaceClient } from './workspaceClient';
+
+export type ServiceClient = {
+  id: ID;
+  name: string;
+  description?: Maybe<string>;
+  endpointUrl: string;
+  aggregatedServices?: Array<Maybe<ID>>;
+  getKinds: () => Promise<Array<AssistantKindsFragment>>;
+  getFunctions: () => Promise<Array<AssistantFunctionsFragment>>;
+  getWorkspace: () => Maybe<WorkspaceClient>;
+};
