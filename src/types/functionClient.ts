8940a907916a014a4e8c925570646a82820c7f91
Darrell Lewis-Sandy <dlewissandy@gmail.com>
2022-07-07 17:39:18 -0700

    Updated definition of functionClient

--- src/types/functionClient.ts
+++ src/types/functionClient.ts
@@ -1,8 +1,77 @@
+import {
+  FieldModifiers,
+  FieldType,
+  FunctionType,
+  GraphqlOperationType,
+  Maybe,
+  OperationType,
+} from '../schema';
 import { ID } from '../schema/scalars';
+import { JustID, NamedEntity } from './Fragments';
 
 export type FunctionClient = {
   id: ID;
   name: string;
+  description?: Maybe<string>;
+  isDeleted?: boolean;
+  service?: Maybe<NamedEntity>;
+  arguments: Array<{
+    id: ID;
+    name: string;
+    type: FieldType;
+    modifiers?: Maybe<Array<Maybe<FieldModifiers>>>;
+    typeKindId?: Maybe<ID>;
+    kind?: Maybe<{
+      id: ID;
+      name: string;
+      service?: Maybe<NamedEntity>;
+    }>;
+  }>;
+  implementation?: Maybe<{
+    id: ID;
+    entrypoint?: Maybe<JustID>;
+    operations: Array<{
+      id: ID;
+      type: OperationType;
+      function?: Maybe<{
+        id: ID;
+        name: string;
+        service?: Maybe<NamedEntity>;
+      }>;
+      argumentValues: Array<{
+        id: ID;
+        argument?: Maybe<NamedEntity>;
+        operation?: Maybe<JustID>;
+        argumentRef?: Maybe<ID>;
+      }>;
+    }>;
+  }>;
+  isGenerated?: boolean;
+  functionType?: FunctionType;
+  graphqlOperationType?: Maybe<GraphqlOperationType>;
+  outputType?: FieldType;
+  outputKindId?: Maybe<ID>;
+  outputModifiers?: Maybe<Array<Maybe<FieldModifiers>>>;
+  kind?: Maybe<{
+    id: ID;
+    name: string;
+    service?: Maybe<NamedEntity>;
+  }>;
+  graph?: Maybe<{
+    id: ID;
+    lockedBy?: Maybe<string>;
+    nodes?: Maybe<
+      Array<{
+        id: ID;
+        functionGraphNode?: Maybe<{
+          id: ID;
+          operationId: ID;
+        }>;
+        knowledgeGraphNode?: Maybe<JustID>;
+      }>
+    >;
+  }>;
+
   lockedBy: () => {};
   canEdit: () => {};
   setLockedBy: () => {};
