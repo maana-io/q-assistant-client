0da9638711a5938c0ab902c521f3d33330d70f17
Eric Ogden <eogden@sparkcognition.com>
2022-07-19 12:07:51 -0700

    move AssistantAPIClient into clients/ folder

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 // Import all of the exported functions as AssistantAPIClient so that it can be
 // exported as the default.  This is done to keep backwards compatibility with
 // previous versions of the library.
-import { AssistantAPIClient as APIClient } from './AssistantAPIClient';
+import { AssistantAPIClient as APIClient } from './clients/AssistantAPIClient';
 export const AssistantAPIClient = APIClient;
 export default AssistantAPIClient;
 
